1a9add35038759c1b9b1a61a14c4526f6d8295b0
Craig Phillips <cphillips@2u.com>
2019-03-04 08:09:45 -0500

    Updating exports

--- .oh-my-zsh/custom/exports.zsh
+++ .oh-my-zsh/custom/exports.zsh
@@ -4,7 +4,7 @@ export KUBERNETES_PROVIDER=aws
 export KUBE_EDITOR="vim"
 
 # Vault Exports
-export VAULT_ADDR=https://vault.prod.2u.com:8200/
+export VAULT_ADDR=https://vault.main.prod.2u.com:8200/
 export VAULT_CACERT=$HOME/.certs/vault-ca.pem
 export VAULT_AUTH_GITHUB_TOKEN=$(cat ~/.secrets/vault-token)
 
