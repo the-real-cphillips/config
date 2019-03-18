ceb87515e914b55e706c14723ef8fa147bcf5283
Craig Phillips <cphillips@2u.com>
2019-03-18 09:13:47 -0400

    Removing vault GITHUB TOKEN env var export

--- .oh-my-zsh/custom/exports.zsh
+++ .oh-my-zsh/custom/exports.zsh
@@ -6,7 +6,6 @@ export KUBE_EDITOR="vim"
 # Vault Exports
 export VAULT_ADDR=https://vault.main.prod.2u.com:8200/
 export VAULT_CACERT=$HOME/.certs/vault-ca.pem
-export VAULT_AUTH_GITHUB_TOKEN=$(cat ~/.secrets/vault-token)
 
 # Ansible Exports
 export ANSIBLE_INVENTORY=/Users/cphillips/work/ansible/inventory/ec2.py
