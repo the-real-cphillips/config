3c703b9d40c0c9f55ece388adcc38cf5dee1bb54
Craig Phillips <cphillips@2u.com>
2019-11-02 14:10:26 -0400

    Update

--- .oh-my-zsh/custom/exports.zsh
+++ .oh-my-zsh/custom/exports.zsh
@@ -1,15 +1,13 @@
 # Kubernetes Exports
-export KUBECONFIG=/Users/cphillips/.kube/config
+export KUBECONFIG=${HOME}/.kube/config
 export KUBERNETES_PROVIDER=aws
 export KUBE_EDITOR="vim"
 
-# Vault Exports
-export VAULT_ADDR=https://vault.main.prod.2u.com:8200/
-export VAULT_CACERT=$HOME/.certs/vault-ca.pem
-
 # Ansible Exports
-export ANSIBLE_INVENTORY=/Users/cphillips/.ansible/aws_ec2.yml
+export ANSIBLE_INVENTORY=${HOME}/.ansible/aws_ec2.yml
 export ANSIBLE_NOCOWS=1
 
-# Causes issues since High Sierra with forking processes.
-export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
+if [[ $(uname) == 'Darwin']]; then
+  # Causes issues since High Sierra with forking processes.
+  export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
+fi
