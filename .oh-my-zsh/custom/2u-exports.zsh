e92e4120441e3f52f5afb67dfd981ec7dfe8e019
Craig Phillips <cphillips@2u.com>
2021-05-13 10:11:16 -0400

    biting the bullet

--- .oh-my-zsh/custom/2u-exports.zsh
+++
@@ -1,3 +0,0 @@
-# Vault Exports
-export VAULT_ADDR=https://vault.main.prod.2u.com:8200/
-export VAULT_CACERT=${HOME}/.certs/vault-ca.pem
--- tmux-start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Starting TMUX
-$(which tmux) attach -t main || tmux new -s main
